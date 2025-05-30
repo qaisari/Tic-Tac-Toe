07e91c4e7d01be433cf5dcf8835d2fcd16f77bfd
Ayman Qaisari <qaisariayman@gmail.com>
2025-05-30 20:20:07 +0200

    Third commit: adding handleMove() to check if the move is valid or not

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,7 @@
 const prompt = require("prompt-sync")();
 const gameBoard = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-let currentPlayer = "";
+let currentPlayer = "❌";
+let gameActive = true;
 
 function printBoard() {
     console.log(`
@@ -10,4 +11,22 @@ function printBoard() {
         -------------
         ${gameBoard[6]} | ${gameBoard[7]} | ${gameBoard[8]}
         `);
+}
+function handleMove(position) {
+    if (gameBoard[position] === " ") {
+        gameBoard[position] = currentPlayer;
+    } else {
+        console.log("Current position is occupied");
+        return false;
+    }
+    currentPlayer = currentPlayer === "❌" ? "⭕" : "❌";
+}
+while(gameActive) {
+    printBoard();
+    let playerInput = prompt(`Player ${currentPlayer} enter your move (0-8): `);
+    if (playerInput >= 0 && playerInput <= 8) {
+        handleMove(playerInput);    
+    } else {
+        console.log("Invalid move, try again!");
+    }
 }
